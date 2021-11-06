6569ef436151d739cd613f04c720fcdb796bde75
rtinney1 <35842918+rtinney1@users.noreply.github.com>
2021-11-06 19:18:52 -0400

    Cleaned up code. Added detailed results

--- passwordML.py
+++ passwordML.py
@@ -1,52 +1,92 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from collections import Counter
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.exceptions import ConvergenceWarning
+
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 import random
 import sys
 import os
 import math
 
+import warnings
+warnings.simplefilter(action="ignore", category=FutureWarning)
+warnings.simplefilter(action="ignore", category=ConvergenceWarning)
+
+#Uses a list of passwords to verify the ML algorithm works
+def predictMe(modelType, model, vectorizor):
+    headers = ["Password", "Label"]
+    predict = ["ADer1234&^!!!dsdsds", "hello", "wakawaka", "fdsafdadfsadfsdfa", "fdsfdsadsfA", "gtrdeA@!"]
+    xPredict = vectorizer.transform(predict)
+    yPredict = model.predict(xPredict)
+
+    print("\tPrediction of passwords")
+    num = 0
+    printMe = []
+    for password in predict:
+        printMe.append([password, yPredict[num]])
+        num += 1
+
+    print("\t{}".format(pd.DataFrame(headers, printMe)))
+    print("\n")
+
+#Results of the ML algorithms
+def reviewResults(modelType, model, fitted, xTrain, yTrain, xTest, yTest, vectorizer):
+    print(
+    """******************************
+RESULTS for {} 
+******************************""".format(modelType))
+
+    ret = fitted.predict(xTest)
+
+    print("\tAccuracy Score: {}".format(accuracy_score(yTest, ret)))
+    print("\tPrecision Score: {}".format(precision_score(yTest, ret, average="binary", pos_label="bad")))
+    print("\tRecall Score: {}".format(recall_score(yTest, ret, average="binary", pos_label="bad")))
+    print("\tF1 Score: {}".format(f1_score(yTest, ret, average="binary", pos_label="bad")))
+
+    predictMe(modelType, model, vectorizer)
+
+#Logistic Regression
+def LogR(xTrain, yTrain, xTest, yTest, vectorizer):
+    print("Performing Logistic Regression\n")
+    lgs = LogisticRegression()
+    fit = lgs.fit(xTrain, yTrain)
+    reviewResults("Logistic Regression", lgs, fit, xTrain, yTrain, xTest, yTest, vectorizer)
+
+#Custom function to separate each password into it's individual characters
 def getTokens(input):
     #split all characters
-    return list(input)
+    tokens = []
+    for i in input:
+        tokens.append(i)
+    return tokens
 
-def LR(path):
+#Creates the xTrain, yTrain, xTest, yTest data for the models
+def createTrainTestData(path):
+    print("Creating data set\n")
     passwordsCSV = pd.read_csv(path, ',', on_bad_lines="skip", engine="python")
-    passwordsData = pd.DataFrame(passwordsCSV)
 
-    passwordsData = np.array(passwordsData)
-    random.shuffle(passwordsData)
+    yLabels = passwordsCSV["Label"]
+    allPass = passwordsCSV["Password"].values.astype('U')
 
-    y = [d[1] for d in passwordsData]
-    corpus = [d[0] for d in passwordsData]
-    print(y)
-    print(corpus)
-    vectorizer = TfidfVectorizer(tokenizer=getTokens)
-    x = vectorizer.fit_transform(corpus)
+    vectorizer = TfidfVectorizer(tokenizer=getTokens, lowercase=False)
+    x = vectorizer.fit_transform(allPass)
 
-    xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.3, random_state=42)
+    xTrain, xTest, yTrain, yTest = train_test_split(x, yLabels, test_size=0.3, random_state=42)
 
-    lgs = LogisticRegression()
-    lgs.fit(xTrain, yTrain)
-    print("Logistic Regression Prediction Score: {}".format(lgs.score(xTest, yTest)))
-
-    return vectorizer, lgs
-
-
+    return xTrain, yTrain, xTest, yTest, vectorizer
+    
 if __name__ == "__main__":
     path = os.path.join(os.getcwd(), "passworddataset.csv")
 
-    print("Performing Logistic Regression")
-    vectorizer, lgs = LR(path)
+    xTrain, yTrain, xTest, yTest, vectorizer = createTrainTestData(path)
 
-    predict = ["ADer1234&^!!!dsdsds", "hello", "wakawaka"]
-    xPredict = vectorizer.transform(predict)
-    yPredict = lgs.predict(xPredict)
+    LogR(xTrain, yTrain, xTest, yTest, vectorizer)
+    
 
-    print("Prediction of {} from Logistic Regression: {}".format(predict, yPredict))
+    
