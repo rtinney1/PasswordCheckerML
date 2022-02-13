9cd63dd3e17ae7d40cc32e34e21c7947c3d082aa
rtinney1 <35842918+rtinney1@users.noreply.github.com>
2022-02-13 16:18:01 -0500

    Update testPassphrase.py

--- Stage2/testPassphrase.py
+++ Stage2/testPassphrase.py
@@ -4,5 +4,6 @@ import time
 if __name__ == "__main__":
     with open("experimentPass.txt", "r") as f:
         for line in f.readlines():
-            os.system(f"curl -s -o nul -H \"Content-Type: multipart/form-data\" -X POST http://localhost:5000/createPass/ --form \"password={line}\"")
-            time.sleep(5)
+            for x in range(0, 100):
+                os.system(f"curl -s -o nul -H \"Content-Type: multipart/form-data\" -X POST http://localhost:5000/createPass/ --form \"password={line}\"")
+                time.sleep(1)
